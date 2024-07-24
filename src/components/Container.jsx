ed771833cba135239f740ba4f325205ee1981189
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-07-24 17:36:51 +0530

    solved the overlapping issue

---
+++ src/components/Container.jsx
@@ -0,0 +1,52 @@
+import { forwardRef } from 'react'
+import clsx from 'clsx'
+
+const OuterContainer = forwardRef(function OuterContainer(
+  { className, children, ...props },
+  ref
+) {
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        'px-4 sm:px-6 md:px-8 lg:px-12',
+        className
+      )}
+      {...props}
+    >
+      <div className="mx-auto max-w-7xl">{children}</div>
+    </div>
+  )
+})
+
+const InnerContainer = forwardRef(function InnerContainer(
+  { className, children, ...props },
+  ref
+) {
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        'relative px-4 sm:px-6 md:px-8 lg:px-12',
+        className
+      )}
+      {...props}
+    >
+      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+    </div>
+  )
+})
+
+export const Container = forwardRef(function Container(
+  { children, ...props },
+  ref
+) {
+  return (
+    <OuterContainer ref={ref} {...props}>
+      {children}
+    </OuterContainer>
+  )
+})
+
+Container.Outer = OuterContainer
+Container.Inner = InnerContainer
